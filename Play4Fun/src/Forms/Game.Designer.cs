34bf1d904d8071255b4b07341f22fa644a71b4e3
Bevíz László <bevizlaci@gmail.com>
2024-03-01 11:07:25 +0100

    update

--- Play4Fun/src/Forms/Game.Designer.cs
+++ Play4Fun/src/Forms/Game.Designer.cs
@@ -35,6 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(204)))), ((int)(((byte)(214)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Game";
