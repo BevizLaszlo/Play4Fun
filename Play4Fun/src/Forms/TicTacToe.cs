468b5599029fdae0626db5b6b7737ebf1294b067
RevaiErik <revaierik100@gmail.com>
2024-03-31 22:21:02 +0200

    update

--- Play4Fun/src/Forms/TicTacToe.cs
+++ Play4Fun/src/Forms/TicTacToe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Play4Fun.src.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,24 @@ namespace Play4Fun.src.Forms
 {
     public partial class TicTacToe : Form
     {
+        private int playerTurnIndex = 0;
         public TicTacToe()
         {
             InitializeComponent();
+            UpdateContent();
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void UpdateContent() 
+        {
+            playersTurn_label.Text = $"{Gameplay.Instance.Players[playerTurnIndex].Name.ToUpper()}'S TURN";
+            playersTurn_label.ForeColor = Gameplay.Instance.Players[Gameplay.PlayerTurnIndex].BackColor;
         }
     }
 }
